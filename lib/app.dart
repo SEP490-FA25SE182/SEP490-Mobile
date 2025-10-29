101f164c3c09a06d546eb93117ec4aca21776391
NhatMinhz <trinhhoangnhatminh123@gmail.com>
2025-10-29 14:49:17 +0700

    fix: app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sep490_mobile/page/blog/blog_detail_page.dart';
+import 'package:sep490_mobile/page/book/book_detail_page.dart';
 import 'package:sep490_mobile/page/profile/edit_address_page.dart';
 import 'package:sep490_mobile/page/scan_page.dart';
 import 'provider.dart';
